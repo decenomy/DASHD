035ffb1355d2a6c09374aea4886ad8dc2fbb1446
Pedro Simões <pedro@decenomy.net>
2021-02-23 19:33:54 +0000

    minor fix on spork 105

--- src/main.cpp
+++ src/main.cpp
@@ -3735,13 +3735,19 @@ bool ContextualCheckBlock(const CBlock& block, CValidationState& state, CBlockIn
     // }
 
     // Size limits
-    unsigned int nMaxBlockSize = std::min(
-        (unsigned int)sporkManager.GetSporkValue(SPORK_105_MAX_BLOCK_SIZE), 
-        MAX_BLOCK_SIZE_CURRENT
-    );
+    if (nHeight != 0 && !IsInitialBlockDownload()) {
+        unsigned int nMaxBlockSize = std::max(
+            (unsigned int)1000, 
+            std::min(
+                (unsigned int)sporkManager.GetSporkValue(SPORK_105_MAX_BLOCK_SIZE),
+                MAX_BLOCK_SIZE_CURRENT
+            )
+        );
 
-    if (::GetSerializeSize(block, SER_NETWORK, PROTOCOL_VERSION) > nMaxBlockSize)
-        return state.DoS(100, false, REJECT_INVALID, "bad-blk-length", false, "size limits failed");
+        if (::GetSerializeSize(block, SER_NETWORK, PROTOCOL_VERSION) > nMaxBlockSize) {
+            return state.Invalid(error("%s : size limits failed", __func__), REJECT_INVALID, "bad-blk-length");
+        }
+    }
 
     return true;
 }
