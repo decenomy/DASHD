f450cf31c5bf656f8e258caac549d8138ba81b3a
perseu <jmsaguas@gmail.com>
2021-02-24 19:32:21 +0100

    Updating site information.

--- src/qt/locale/pivx_ru.ts
+++ src/qt/locale/pivx_ru.ts
@@ -3837,8 +3837,8 @@ There was an error trying to save the address list to %1. Please try again.</sou
         <translation>2) Почему мои DASHD невозможно потратить?</translation>
     </message>
     <message>
-        <source>https://sappcoin.com/</source>
-        <translation>https://sappcoin.com/</translation>
+        <source>https://dashdiamond.net/</source>
+        <translation>https://dashdiamond.net/</translation>
     </message>
     <message>
         <source>1</source>
